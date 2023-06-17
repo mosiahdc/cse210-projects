1265f53b13add46c5032714e30ceea864f6a17ca
mosiahdc <contact.mosiah1994@gmail.com>
2023-06-18 05:00:45 +0800

    Finishi Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int inputNumber = -1;
+
+        while (inputNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            inputNumber = int.Parse(Console.ReadLine());
+
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+        }
+
+        int sum = 0;
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        float average = (float)sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
