33db15e37e95d6799f8d0e4630573386d5044633
mosiahdc <contact.mosiah1994@gmail.com>
2023-06-29 15:30:37 +0800

    Finish Prove 3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,17 +7,24 @@ public class Program
     {
 
         string userInput = "";
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+        Scripture scripture = new Scripture();
 
-        while (userInput != "quit")
-        {
+        scripture.LoadFromFile();
+        do{
             Console.WriteLine(scripture.GetDisplayText());
             Console.WriteLine();
             Console.WriteLine("Press enter to continue or type 'quit' to finish");
             userInput = Console.ReadLine();
-            scripture.HideRandomWords(5);
+            if (scripture.IsCompletelyHidden() == true)
+            {
+                break;
+            }
+            else
+            {
+                scripture.GenerateRandomNumber();
+            }
         }
+        while (userInput != "quit");
     }   
 
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -4,10 +4,16 @@ public class Scripture{
 
     private List<Word> _words;
 
-    private List<int> _wordsIndex;
+    private List<int> _wordsIndex = new List<int>();
 
-    public Scripture(Reference reference, string text)
+    //Scripture scripture;
+
+    public Scripture()
     {
+
+    }
+    public Scripture(Reference reference, string text)
+    {   
         _reference = reference;
         _words = new List<Word>();
 
@@ -17,25 +23,34 @@ public class Scripture{
             Word newWord = new Word(wordString);
             _words.Add(newWord);
         }
+        
     }
 
     public void GenerateRandomNumber()
     {   
-        //Random random = new Random();
-        //int randomNumber = 0;
-        //randomNumber= random.Next(_words.Count);
-//
-        //HideRandomWords(5);
+
+        Random random = new Random();
+
+        int randomNumber;
+        do{
+            randomNumber = random.Next(_words.Count);
+        }
+        while (_wordsIndex.Contains(randomNumber));
+        
+        HideRandomWords(randomNumber);
+
     }
 
     public void HideRandomWords(int numberToHide)
-    {
+    {   
+        _wordsIndex.Add(numberToHide);
         Word hiddenText = _words[numberToHide];
         hiddenText.Hide();
     }
 
     public string GetDisplayText()
     {   
+
         string scriptureText = "";
 
         foreach (Word word in _words)
@@ -54,7 +69,53 @@ public class Scripture{
     }
 
     public bool IsCompletelyHidden()
+    {   
+        bool allWordsHidden = false;
+
+
+        if (_wordsIndex.Count == _words.Count)
+        {
+            allWordsHidden = true;
+        }
+
+        return allWordsHidden;
+    }
+
+    public void LoadFromFile()
     {
-        return true;
+        string[] lines = System.IO.File.ReadAllLines("scriptures.txt");
+
+        Random random = new Random();
+
+        int randomLine, lineNumber;
+        randomLine = random.Next(lines.Count());
+
+        if (randomLine % 2 == 0)
+        {
+            lineNumber = randomLine;
+        }
+        else
+        {
+            lineNumber = randomLine - 1;
+        }
+
+        string[] scriptureReference = lines[lineNumber].Split("/");
+        string book = scriptureReference[0];
+        int chapter = Int32.Parse(scriptureReference[1]);
+        int verse = Int32.Parse(scriptureReference[2]);
+        
+        if (scriptureReference.Count() == 4)
+        {
+            int endVerse = Int32.Parse(scriptureReference[3]);
+            _reference = new Reference(book, chapter, verse, endVerse);
+        }
+        else
+        {
+            _reference = new Reference(book, chapter, verse);
+        }
+            
+        string scripturePassage = lines[lineNumber + 1];
+        Scripture scripture = new Scripture(_reference, scripturePassage);
+        this._words = scripture._words;
     }
 }
